ff9d30ca3f3a7de543033dac8d42a9be8fa76311
Lucky <lhlongwane81@gmail.com>
2023-04-18 18:28:03 +0200

    first commit

---
+++ js/app.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    //HERO Slider
+    $('#hero-slider').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
+        items: 1,
+        dots: false,
+        smartSpeed: 1000,
+        navText : ['PERV', 'NEXT'],
+        responsive:{
+            0:{
+              
+            },
+            600:{
+                
+            },
+            1000:{
+                
+            },
+        }
+    })
+});
